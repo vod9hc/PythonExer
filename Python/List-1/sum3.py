4e1194a36b7ce8c80f2a940e4d47415708c33107
vod9hc <84865169+vod9hc@users.noreply.github.com>
2021-07-15 23:22:43 +0700

    Create sum3.py

---
+++ Python/List-1/sum3.py
@@ -0,0 +1,12 @@
+#Given an array of ints length 3, return the sum of all the elements.
+
+
+#sum3([1, 2, 3]) → 6
+#sum3([5, 11, 2]) → 18
+#sum3([7, 0, 0]) → 7
+
+def sum3(nums):
+  total = 0
+  for i in range(0, len(nums)):
+    total += nums[i]
+  return total
